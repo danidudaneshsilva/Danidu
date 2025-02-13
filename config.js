67b62248a9b5b062d6b15678778e1fc957bb075d
danidudaneshsilva <topg17000@gmail.com>
2025-02-13 11:41:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -37,10 +37,10 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
   //  සෙසන් අයිඩී එක දාන්න👇👇👇 (උඩු කොමා දෙක ඇතුලේ)
-  SESSION_ID: process.env.SESSION_ID || "", 
+  SESSION_ID: process.env.SESSION_ID |Golden-Queen-MD-NlpFTKbb#6Fx4m9h3jtrm1R2NO-VoEO8vBz7JeF10DhNwV3cQ5wAftrc| "", 
 
 //  Mongodp URl එක දාන්න 👇👇👇 (උඩු කොමා දෙක ඇතුලේ )
-  MONGODB: process.env.MONGODB || "",
+  MONGODB: process.env.MONGODB || "mongosh "mongodb+srv://cluster0.nxltl.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509" --apiVersion 1 --tls --tlsCertificateKeyFile <path to PEM file>",
 
 
 MAX_SIZE: 500,
